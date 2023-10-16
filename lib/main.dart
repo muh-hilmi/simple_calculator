8a3cd4b8b0b520b79f9c096900de76a88d0ed458
muhhilmi <mailhilmi@gmail.com>
2023-10-16 11:11:05 +0700

    delete main were not usable

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,39 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(CalculatorApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class CalculatorApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Calculator(),
+      debugShowCheckedModeBanner: false,
+      home: Kalkulator(),
     );
   }
 }
 
-class Calculator extends StatefulWidget {
+class Kalkulator extends StatefulWidget {
+  Kalkulator({super.key});
+
   @override
-  _CalculatorState createState() => _CalculatorState();
+  State<Kalkulator> createState() => _KalkulatorState();
 }
 
-class _CalculatorState extends State<Calculator> {
-  String _output = "0";
-  double num1 = 0;
-  double num2 = 0;
-  String operand = "";
+class _KalkulatorState extends State<Kalkulator> {
+  var size, height, width, _output, angka, operand, num1, num2;
+  @override
+  void initState() {
+    super.initState();
+    _output = "0"; // Inisialisasi hasil menjadi "0".
+  }
 
-  void buttonPressed(String buttonText) {
+  button(var buttonText) {
     if (buttonText == "C") {
       _output = "0";
       num1 = 0;
@@ -30,30 +41,32 @@ class _CalculatorState extends State<Calculator> {
       operand = "";
     } else if (buttonText == "+" ||
         buttonText == "-" ||
-        buttonText == "*" ||
+        buttonText == "X" ||
         buttonText == "/") {
       num1 = double.parse(_output);
       operand = buttonText;
-      _output = "0";
+      _output = "";
     } else if (buttonText == "=") {
       num2 = double.parse(_output);
       if (operand == "+") {
         _output = (num1 + num2).toString();
-      }
-      if (operand == "-") {
+      } else if (operand == "-") {
         _output = (num1 - num2).toString();
-      }
-      if (operand == "*") {
+      } else if (operand == "X") {
         _output = (num1 * num2).toString();
-      }
-      if (operand == "/") {
+      } else if (operand == "/") {
         _output = (num1 / num2).toString();
       }
       num1 = 0;
       num2 = 0;
       operand = "";
     } else {
-      _output = _output + buttonText;
+      // Menggunakan buttonText sebagai angka, bukan operator.
+      if (_output == "0") {
+        _output = buttonText;
+      } else {
+        _output = _output + buttonText;
+      }
     }
 
     setState(() {
@@ -61,80 +74,300 @@ class _CalculatorState extends State<Calculator> {
     });
   }
 
-  Widget buildButton(String buttonText) {
-    return Expanded(
-      child: OutlinedButton(
-        // padding: EdgeInsets.all(24.0),
-        onPressed: () => buttonPressed(buttonText),
-        child: Text(
-          buttonText,
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
+    height = size.height;
+    width = size.width;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Calculator'),
-      ),
+      appBar: AppBar(title: const Text("Kalkulator Sederhana")),
       body: Column(
-        children: <Widget>[
+        children: [
           Container(
             alignment: Alignment.centerRight,
-            padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+            padding: EdgeInsets.symmetric(vertical: height / 10),
             child: Text(
               _output,
-              style: TextStyle(
-                fontSize: 48.0,
+              style: const TextStyle(
+                fontSize: 48,
                 fontWeight: FontWeight.bold,
               ),
             ),
           ),
-          Expanded(
-            child: Divider(),
+          Container(
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => button("7"),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "7",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => button("8"),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "8",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => button("9"),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "9",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => button("X"),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "X",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => button("4"),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "4",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => button("5"),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "5",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => button("6"),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "6",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => button("-"),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "-",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => button("1"),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "1",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => button("2"),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "2",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => button("3"),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "3",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => button("+"),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "+",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => button("C"),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "C",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => button("0"),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "0",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => button("="),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "=",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => button("/"),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(height / 5, width / 5),
+                        backgroundColor: Colors.primaries[
+                            Random().nextInt(Colors.primaries.length)],
+                      ),
+                      child: const Text(
+                        "/",
+                        style: TextStyle(
+                          fontSize: 56,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-          Column(
-            children: [
-              Row(
-                children: [
-                  buildButton("7"),
-                  buildButton("8"),
-                  buildButton("9"),
-                  buildButton("/"),
-                ],
-              ),
-              Row(
-                children: [
-                  buildButton("4"),
-                  buildButton("5"),
-                  buildButton("6"),
-                  buildButton("*"),
-                ],
-              ),
-              Row(
-                children: [
-                  buildButton("1"),
-                  buildButton("2"),
-                  buildButton("3"),
-                  buildButton("-"),
-                ],
-              ),
-              Row(
-                children: [
-                  buildButton("C"),
-                  buildButton("0"),
-                  buildButton("="),
-                  buildButton("+"),
-                ],
-              ),
-            ],
-          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context, "/");
+              },
+              child: const Text("Back"))
         ],
       ),
     );
